24ad7bd617240f200fe1be0d28dc213fc5ff4606
Maryammohd-hub <maryammohd.aseeri@gmail.com>
2026-03-24 03:58:15 +0300

    created the structure

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -9,7 +9,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: MyHomePage(),
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -23,6 +33,56 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green, // Button background color
+                foregroundColor: Colors.white, // Text (and ripple) color
+                padding: const EdgeInsets.all(16.0),
+              ),
+              onPressed: () {
+                // The user picked true.
+              },
+              child: const Text('True', style: TextStyle(fontSize: 20.0)),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red, // Button background color
+                foregroundColor: Colors.white, // Text (and ripple) color
+                padding: const EdgeInsets.all(16.0),
+              ),
+              onPressed: () {
+                // The user picked true.
+              },
+              child: const Text('false', style: TextStyle(fontSize: 20.0)),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
